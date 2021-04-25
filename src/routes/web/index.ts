73dcf452a3db68626c79af51a0d1b584bb6afb18
Ali Dalal <ali.dalal92@gmail.com>
2021-04-26 01:32:34 +0800

    remove unused modules

--- src/routes/web/index.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express';
-import { getCitiesWithProductAndBrochures } from '@controllers/cities';
-
-const webRoutes = express.Router();
-
-webRoutes.get('/:city_slug/:product_slug', getCitiesWithProductAndBrochures);
-
-export default webRoutes;
