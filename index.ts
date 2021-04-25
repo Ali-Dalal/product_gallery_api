e8bc3c78409a8c6423ddb445614609ec3c3f80db
Ali Dalal <ali.dalal92@gmail.com>
2021-04-26 01:29:37 +0800

    fix ci

--- index.ts
+++ index.ts
@@ -1,13 +1,9 @@
 import Server from './src/bin/www';
 import configuration from '@config';
-import configBuilder from '@config/builder';
 import * as Logger from '@logger';
 
 const port = configuration.PORT;
 
-(async (): Promise<void> => {
-    await configBuilder();
-    Server.listen(Number(port), '0.0.0.0', async () => {
-        Logger.write(`Service started on port ${port}`);
-    });
-})();
+Server.listen(Number(port), '0.0.0.0', async () => {
+    Logger.write(`Service started on port ${port}`);
+});
